d1229aac4f08533cc89a7ed396707786ae2e87aa
Zach1281 <zachary_romero@yahoo.com>
2023-08-16 16:46:24 -0700

    Finish exercise-02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, times2Repeat) {
+    if(times2Repeat < 0){return "ERROR";}
+    let repeatedString = "";
+    for(let i = 0; i < times2Repeat; i++){
+        repeatedString += string;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
