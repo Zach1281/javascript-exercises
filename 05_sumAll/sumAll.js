259ca5b1bed3ec42bf99cc57e10b5fc5974fad51
Zach1281 <zachary_romero@yahoo.com>
2023-08-17 14:36:52 -0700

    Complete exercise-05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(numStart, numEnd) {
+    if((numStart < 0 || numEnd < 0) || !Number.isInteger(numStart) || !Number.isInteger(numEnd)){ return "ERROR";}
+    if(numStart > numEnd){
+        let swap = numStart;
+        numStart = numEnd;
+        numEnd = swap;
+    }
+    let sum = 0;
+    for(let i = numStart; i <= numEnd; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
