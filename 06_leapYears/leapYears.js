a0343baa85771864d208b8c70d498be1ede89c8f
Zach1281 <zachary_romero@yahoo.com>
2023-08-17 15:02:16 -0700

    Complete exercise-06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if(year < 0 || !Number.isInteger(year)){return "ERROR";}
+    // a leap year is any year that is divisible by four
+    // by default most years are not leap years
+    // any year that is divisible by 100 is not a leap year unless it is divisible by 400 then it is one
+    
+    if(year % 100 === 0){
+        if(year % 400 === 0){
+            return true;
+        }
+        return false;
+    }
 
+    if(year % 4 === 0){return true;}
+    return false;
 };
 
 // Do not edit below this line
