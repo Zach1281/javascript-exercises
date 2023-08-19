2f51a7e01061a2769281cec04f28c9d57cb9030b
Zach1281 <zachary_romero@yahoo.com>
2023-08-19 15:01:54 -0700

    Complete exercise-09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    string = string.replace(/[^a-zA-Z0-9]+/g, '');
+    let split = string.split("");
+    let reverse = split.reverse();
+    let join = reverse.join("");
+    if(string === join)
+        return true;
+    else
+        return false;
 };
 
 // Do not edit below this line
